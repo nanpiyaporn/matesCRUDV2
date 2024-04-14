8db0dcec55fde3b3d1d67e1135082501b5d520f1
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-14 17:30:03 -0400

    Link .env file and update import statement in Home.js

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,15 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+
+
+const supabaseUrl = "https://zztckcckxbczjnjebicw.supabase.co"
+//process.env.REACT_APP_SUPABASE_URL
+
+
+//const supabaseKey = import.meta.env.REACT_APP_SUPABASE_ANON_KEY
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default supabase
+export default supabase;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,50 @@
 import supabase from "../client"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("data")
+          .select("*")
+
+        if (error) {
+          setFetchError(error.message)
+          setData(null)
+          console.error("error", error)
+          return
+        }
+        setData(data)
+        setFetchError(null)
+      } catch (error) {
+        setFetchError(error.message)
+      }
+    }
+    fetchData()
+
+  }, [])
+
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p className="fetch-error">{fetchError}</p>}
+      {data && (
+        <div className="crewmetes">
+          {data.map((crewmate) => (
+            <div key={crewmate.id} className="crewmate">
+              <h2>{crewmate.name}</h2>
+              <h2>{crewmate.title}</h2>
+              <p>{crewmate.rating}</p>
+              <p>{crewmate.color}</p>
+            </div>
+          ))}
+        </div>
+
+      )}
     </div>
   )
 }
