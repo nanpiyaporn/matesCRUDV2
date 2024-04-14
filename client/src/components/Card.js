eb5a8b1eb1e9f191d670d88ddbdc65872f2ff6fd
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-14 19:26:40 -0400

    Refactor Card component to use 'crewmate' prop instead of 'crewmates'

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 
-const Card =({crewmates}) => {
+const Card =({crewmate}) => {
     return (
         <div className = "crewmate-card">
-            <p>{crewmates.id}</p>
-            <h2>{crewmates.name}</h2>
-            <h2>{crewmates.title}</h2>
-            <p>{crewmates.color}</p>
-            <div className = "rating"> {crewmates.rating}</div>
+            <p>{crewmate.id}</p>
+            <h2>{crewmate.name}</h2>
+            <h4>{crewmate.title}</h4>
+            <p>{crewmate.color}</p>
+            <div className = "rating"> {crewmate.rating}</div>
         </div>
     )
 }
--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,7 +1,41 @@
 const Create = () => {
+
+  const [ title, setTitle ] = useState('')
+  const [ name, setName ] = useState('')
+  const [ color, setColor ] = useState('')
+  const [ rating, setRating ] = useState('')
+  const [ formError, setError ] = useState(null)
+
+
   return (
     <div className="page create">
-      <h2>Create</h2>
+      <form onSubmit ={handleSubmit}>
+        <label htmlFor = "name">Name</label>
+        <input 
+          type = "text" 
+          id = "name" value = {name} onChange = {e => setName(e.target.value)} 
+          />
+
+        <label htmlFor = "title">Title</label>
+        <input 
+          type = "text" 
+          id = "title" value = {title} onChange = {e => setTitle(e.target.value)} 
+          />
+
+        <label htmlFor = "color">Color</label>
+        <input 
+          type = "text" 
+          id = "color" value = {color} onChange = {e => setColor(e.target.value)} 
+          />
+
+        <label htmlFor = "rating">Rating</label>
+        <input 
+          type = "number" 
+          id = "rating" value = {rating} onChange = {e => setRating(e.target.value)} 
+          />
+        <button type = "submit">Create</button>
+        
+      </form>
     </div>
   )
 }
